f54809bf735631f37ae94acbafa6b14ae3bee14f
Mein <saisainghminetun@gmail.com>
2024-01-09 16:36:57 +0630

    Redesign to my like

--- app/src/main/java/it/saimao/shantranslit/MainActivity.kt
+++ app/src/main/java/it/saimao/shantranslit/MainActivity.kt
@@ -5,16 +5,21 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import it.saimao.shantranslit.utils.ShanTranslit
 
 class MainActivity : AppCompatActivity() {
     private lateinit var input: EditText
     private lateinit var output: EditText
+    private lateinit var rbS2E: RadioButton
+    private lateinit var rbE2S: RadioButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         input = findViewById(R.id.edInput)
         output = findViewById(R.id.edOutput)
+        rbS2E = findViewById(R.id.rbShanToEng)
+        rbE2S = findViewById(R.id.rbEngToShan)
         val btConvert: Button = findViewById(R.id.btConvert)
         val btClear: Button = findViewById(R.id.btClear)
         btConvert.setOnClickListener(this::convert)
@@ -24,7 +29,9 @@ class MainActivity : AppCompatActivity() {
     private fun convert(view: View?) {
 
         if (input.text.isNotEmpty()) {
-            output.setText(ShanTranslit.taiToEng(input.text.toString()))
+            val result: String =
+                if (rbS2E.isChecked) ShanTranslit.taiToEng(input.text.toString()) else ShanTranslit.engToTai(input.text.toString())
+            output.setText(result)
         }
 
     }
